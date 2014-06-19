9fd43b5159ee910b17d51742f573dcf7e2fe43cd
Patric <patric.lessmann@gmail.com>
2014-06-19 21:13:14 +0200

    Revert "Intermediate version"
    This reverts commit 17a73598255a25b3cbc8132d873f4d9654853b12.

--- GeoErde/src/de/tuc/emla/geoerde/AboutFragment.java
+++ GeoErde/src/de/tuc/emla/geoerde/AboutFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class AboutFragment extends Fragment implements OnClickListener
 {
@@ -23,26 +23,26 @@ public class AboutFragment extends Fragment implements OnClickListener
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		TextView tv;
+		Button btn;
 		
 		container.removeAllViews();
 		
 		View view = inflater.inflate(R.layout.fragment_about, container, false);
 				
-		tv = (TextView) view.findViewById(R.id.textAbout);
-		tv.setOnClickListener(this);
+		btn = (Button) view.findViewById(R.id.buttonAbout);
+        btn.setOnClickListener(this);
         
         return view;
 	}
 	
 	/**
-     * The eventHandler for the fragment screen
+     * The eventHandler for the statistics-button
      * @param view the view of the main activity
      */
 	@Override
     public void onClick(View v)
 	{
-		if(D) Log.d(LOGCAT, "Exit pressed...");
+		if(D) Log.d(LOGCAT, "Exit button pressed...");
 		FragmentManager fm = getFragmentManager();
 	    if (fm.getBackStackEntryCount() > 0)
 	    {
