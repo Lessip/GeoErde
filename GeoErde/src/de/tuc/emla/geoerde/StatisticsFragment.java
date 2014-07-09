2d7aaabe102df197c56d0edaa8c5c93ab34357c4
Patric <patric.lessmann@gmail.com>
2014-07-09 14:44:05 +0200

    Intermediate state - show statistics
    Necessary commit to upload the other changes

--- GeoErde/src/de/tuc/emla/geoerde/StatisticsFragment.java
+++ GeoErde/src/de/tuc/emla/geoerde/StatisticsFragment.java
@@ -3,15 +3,21 @@ package de.tuc.emla.geoerde;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.database.Cursor;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class StatisticsFragment extends Fragment implements OnClickListener
 {
+	DatabaseController db;
+	LinearLayout llayout;
+	ListView resultsView;
+	
 	public StatisticsFragment()
     {
     }
@@ -23,14 +29,35 @@ public class StatisticsFragment extends Fragment implements OnClickListener
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		LinearLayout llayout;
+		db = ((MainActivity)getActivity()).db;
 		
 		View view = inflater.inflate(R.layout.fragment_statistics, container, false);
 				
 		llayout = (LinearLayout) view.findViewById(R.id.layout_statistics);
 		llayout.setOnClickListener(this);
+		
+		resultsView = (ListView) view.findViewById(R.id.listViewResults);
         
         return view;
+	}
+	
+	/**
+     * A method to read the results from the local database and display them in a listView
+     */
+	public void showResults()
+	{
+		// read the results from the database
+		Cursor resultCursor = db.getGeneralResult();
+		if(resultCursor != null)
+		{
+			resultCursor.moveToFirst();
+			
+			while(!resultCursor.isAfterLast())
+			{
+				// TODO: insert result into resultsView
+				resultCursor.moveToNext();
+			}
+		}
 	}
 	
 	/**
